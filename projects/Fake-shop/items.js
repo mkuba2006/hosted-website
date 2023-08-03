a951fbffcdf94086a35e4e76e1ab66dfb0aee86c
Jakub Myrta <mkuba2006@gmail.com>
2023-08-04 00:09:55 +0200

    04.08(big update)

--- projects/Fake-shop/items.js
+++ projects/Fake-shop/items.js
@@ -3,7 +3,6 @@ products = [
         id:0,
         nazwa_albumu: 'Kid A',
         rok_wydania: '2000',
-        cena: '21.99',
         zespol: 'RadioHead',
         obraz: 'https://a.allegroimg.com/s1024/0c8385/c8405d32460eb53110ca854f0128',
         opis: 'The groundbreaking album by Radiohead, defied conventions and pushed the boundaries of experimental rock, leaving an indelible mark on music history.',
@@ -13,7 +12,6 @@ products = [
         id:1,
         nazwa_albumu: 'OK Computer',
         rok_wydania: '1997',
-        cena: '19.99',
         zespol: 'RadioHead',
         obraz: 'https://ecsmedia.pl/c/ok-computer-w-iext38754044.jpg',
         opis: 'The iconic album by Radiohead, captured the disillusionment of the digital age with its haunting melodies and thought-provoking lyrics, solidifying its place as a timeless masterpiece',
@@ -23,7 +21,6 @@ products = [
         id:2,
         nazwa_albumu: 'The Bends',
         rok_wydania: '1995',
-        cena: '19.99',
         zespol: 'RadioHead',
         obraz: 'https://ecsmedia.pl/c/the-bends-b-iext120934854.jpg',
         opis: 'A  seminal album by Radiohead, showcased the bands evolution into introspective and melodic brilliance, leaving an enduring impact on alternative rock',
@@ -43,7 +40,6 @@ products = [
         id:3,
         nazwa_albumu: 'Dookie',
         rok_wydania: '1994',
-        cena: '25.99',
         zespol: 'Green Day',
         obraz: 'https://lastfm.freetls.fastly.net/i/u/500x500/2248e72411992639ffa8ab94ba97a631.jpg',
         opis: 'Explosive punk rock opus, defined a generation with its catchy hooks, rebellious spirit, and unapologetic energy.',
@@ -53,7 +49,6 @@ products = [
         id:4,
         nazwa_albumu: 'Nimrod',
         rok_wydania: '1997',
-        cena: '30.99',
         zespol: 'Green Day',
         obraz: 'https://m.media-amazon.com/images/I/51UudDglsRL.jpg',
         opis: 'Dynamic and eclectic album, pushed the boundaries of their punk roots, delivering a diverse musical journey filled with raw emotion and infectious anthems.',
@@ -63,7 +58,6 @@ products = [
         id:5,
         nazwa_albumu: 'American Idiot',
         rok_wydania: '2004',
-        cena: '24.99',
         zespol: 'Green Day',
         obraz: 'https://ecsmedia.pl/c/american-idiot-w-iext120304985.jpg',
         opis: 'Politically-charged rock opera, resonated with a generation, blending infectious melodies, punk rock ferocity, and a poignant social commentary into an unforgettable album.',
@@ -81,7 +75,6 @@ products = [
         id:6,
         nazwa_albumu: 'Nevermind',
         rok_wydania: '1991',
-        cena: '49.99',
         zespol: 'Nirvana',
         obraz: 'https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg',
         opis: 'Genre-defining album, ignited the grunge revolution with its raw and cathartic sound, forever shaping the course of alternative rock.',
@@ -91,7 +84,6 @@ products = [
         id:7,
         nazwa_albumu: 'In Utero',
         rok_wydania: '1993',
-        cena: '39.99',
         zespol: 'Nirvana',
         obraz: 'https://m.media-amazon.com/images/I/710XKXHIeNL._UF350,350_QL50_.jpg',
         opis: 'Uncompromising and raw masterpiece, delved into dark introspection and grappled with fame, solidifying its place as a hauntingly authentic portrayal of Kurt Cobains tumultuous emotions.',
@@ -101,7 +93,6 @@ products = [
         id:8,
         nazwa_albumu: 'MTV Unplugged',
         rok_wydania: '1994',
-        cena: '39.99',
         zespol: 'Nirvana',
         obraz: 'https://i.scdn.co/image/ab67616d0000b27336c5417732e53e23cb219246',
         opis: 'Iconic acoustic performance, revealed a stripped-down and intimate side of the band, showcasing their musical versatility and raw emotional depth in an unforgettable live album.',
@@ -120,7 +111,6 @@ products = [
         id:9,
         nazwa_albumu: 'Weezer',
         rok_wydania: '2004',
-        cena: '19.99',
         zespol: 'Weezer',
         obraz: 'https://ecsmedia.pl/c/the-blue-album-w-iext125428022.jpg',
         opis: 'Affectionately known as the Blue Album, introduced an infectious blend of catchy hooks and introspective lyrics that became an anthem for a generation of alternative rock fans.',
@@ -130,7 +120,6 @@ products = [
         id:10,
         nazwa_albumu: 'Pinkerton',
         rok_wydania: '1996',
-        cena: '27.99',
         zespol: 'Weezer',
         obraz: 'https://upload.wikimedia.org/wikipedia/en/c/ca/Pinkerton_cover.jpg',
         opis: 'Emotionally raw and confessional sophomore album, gained a cult following for its unfiltered introspection and unapologetic vulnerability. Definetly 10/10',
@@ -140,11 +129,117 @@ products = [
         id:11,
         nazwa_albumu: 'Maladroit',
         rok_wydania: '2002',
-        cena: '17.99',
         zespol: 'Weezer',
         obraz: 'https://upload.wikimedia.org/wikipedia/en/a/a6/Maladroitweezer.jpg',
         opis: ' Dynamic and energetic album, showcased their return to a heavier rock sound, serving up a collection of catchy hooks and riff-driven anthems that left fans craving more.',
         rank: '8.5/10'
     },
 
+
+
+
+
+
+
+
+
+
+    {
+        id:12,
+        nazwa_albumu: 'Rage Against The Machine',
+        rok_wydania: '1992',
+        zespol: 'Rage Against The Machine',
+        obraz: 'https://upload.wikimedia.org/wikipedia/en/1/1a/RageAgainsttheMachineRageAgainsttheMachine.jpg',
+        opis: 'Provocative lyrics, messages galore. Heavy guitars, energy untamed, Challenging systems, leaving none unnamed. Timeless classic, voices unchained, Revolutionary spirit, forever retained. ',
+        rank: '9/10'
+    },
+
+    {
+        id:13,
+        nazwa_albumu: 'Evil Empire',
+        rok_wydania: '1996',
+        zespol: 'Rage Against The Machine',
+        obraz: 'https://upload.wikimedia.org/wikipedia/en/4/45/Rage_Against_the_Machine_-_Evil_Empire.png',
+        opis: 'Against injustice, they amplify. Raucous riffs, and vocals bold, Protest anthems, their stories told. Defiance unleashed, a fire untold.',
+        rank: '9/10'
+    },
+    {
+        id:14,
+        nazwa_albumu: 'Renegates',
+        rok_wydania: '2000',
+        zespol: 'Rage Against The Machine',
+        obraz: 'https://upload.wikimedia.org/wikipedia/en/5/54/Ratm_renegades.png',
+        opis: 'Revolutionary renditions, their resurrection. Rebel spirits, songs reclaimed, Defiant homage, the bands aim. Renegades rebellion, Rage untamed.',
+        rank: '9/10'
+    },
+
+
+
+
+
+
+    {
+        id: 15,
+        nazwa_albumu: 'Master of Puppets',
+        rok_wydania: '1986',
+        zespol: 'Metallica',
+        obraz: 'https://upload.wikimedia.org/wikipedia/en/b/b2/Metallica_-_Master_of_Puppets_cover.jpg',
+        opis: 'A masterpiece of heavy metal, showcasing the bands musical prowess and iconic sound.',
+        rank: '10/10'
+        },
+        
+        {
+        id: 16,
+        nazwa_albumu: 'Ride the Lightning',
+        rok_wydania: '1984',
+        zespol: 'Metallica',
+        obraz: 'https://upload.wikimedia.org/wikipedia/en/f/f4/Ridetl.png',
+        opis: 'Electrifying riffs and thunderous drums, this album solidified Metallicas place in metal history.',
+        rank: '9.5/10'
+        },
+        
+        {
+        id: 17,
+        nazwa_albumu: 'And Justice for All',
+        rok_wydania: '1988',
+        zespol: 'Metallica',
+        obraz: 'https://m.media-amazon.com/images/I/81pchxSIApL.__AC_SX300_SY300_QL70_ML2_.jpg',
+        opis: 'A politically charged album, showcasing the bands technical proficiency and social commentary.',
+        rank: '9/10'
+        },
+
+
+
+
+
+
+        {
+            id: 20,
+            nazwa_albumu: 'Whatever People Say I Am, Thats What Im Not',
+            rok_wydania: '2006',
+            zespol: 'Arctic Monkeys',
+            obraz: 'https://ecsmedia.pl/c/whatever-people-say-i-am-that-s-what-i-m-not-w-iext73265781.jpg',
+            opis: 'A groundbreaking debut album, capturing the indie-rock spirit of the 2000s with catchy hooks and clever lyrics.',
+            rank: '9.5/10'
+            },
+            
+            {
+            id: 21,
+            nazwa_albumu: 'AM',
+            rok_wydania: '2013',
+            zespol: 'Arctic Monkeys',
+            obraz: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/%22AM%22_%28Arctic_Monkeys%29.jpg',
+            opis: 'A sultry and mature album, blending rock and R&B influences, showcasing the bands evolving musical style.',
+            rank: '9/10'
+            },
+            
+            {
+            id: 22,
+            nazwa_albumu: 'Favourite Worst Nightmare',
+            rok_wydania: '2007',
+            zespol: 'Arctic Monkeys',
+            obraz: 'https://ecsmedia.pl/c/favourite-worst-nightmare-w-iext103296263.jpg',
+            opis: 'An explosive follow-up to their debut, maintaining their energetic sound with a dose of lyrical introspection.',
+            rank: '8.5/10'
+            }
 ]
--- projects/Fake-shop/summary.js
+++ projects/Fake-shop/summary.js
@@ -4,9 +4,10 @@ const navbar = document.querySelector('.navbar');
 const dropdown = document.querySelector("body > nav > div > ul");
 
 
-const image = document.querySelectorAll("section div.card-container .image-container")
+const image = document.querySelectorAll("section div.card-container .card-img-for")
 const icons = document.querySelectorAll("body section div.card-container a i") ;
 for(let i=0;i<image.length; i++){
+    icons[i].style.opacity='0';
     image[i].addEventListener('mouseenter',()=>{
         icons[i].style.opacity='1';
     });
@@ -21,9 +22,11 @@ for(let i=0;i<image.length; i++){
 
 for (let i = 0; i < icons.length; i++) {
     const parent = icons[i].parentElement;
+
     const img = parent.querySelector('section').innerHTML;
     const ppar = parent.parentElement;
-    const txt = ppar.querySelector('.main h1 a').textContent;
+    const pppar = ppar.parentElement;
+    const txt = pppar.querySelector('h1 a').textContent;
     const txtNode = document.createTextNode(txt).textContent;
     const t = document.createElement('p');
     t.innerHTML = txtNode;
@@ -52,6 +55,7 @@ for (let i = 0; i < icons.length; i++) {
 
 
 i.forEach((a)=>{
+    console.log(a);
     const copy = document.createElement('li');
     copy.className = 'li_fav';
 
@@ -83,3 +87,48 @@ i.forEach((a)=>{
     })
 
 })
+
+
+if(window.innerWidth < 601){
+    const title = document.getElementById("site");
+    const arrow = document.createElement('div');
+    arrow.id='arrow'
+    arrow.innerHTML='<i class="fa-solid fa-caret-right"></i>';
+    title.append(arrow);
+    let rotationState = 0;
+    arrow.style.transition = "transform 0.3s ease";
+
+    const search = document.getElementById("before-search");
+    const list = document.getElementById("drp");
+    const nav = document.getElementById('nvb');
+    const products =document.getElementById('products');
+    const h = nav.clientHeight;
+    products.style.top = h - 50+'px';
+    nav.style.boxShadow='none';
+
+    search.style.display = 'block';
+    search.style.transform = 'translateY(-90px)';
+    search.style.transition = 'transform 0.5s ease';
+
+    list.style.display = 'none';
+    // list.style.transform = 'translateY(-120px)';
+    // list.style.transition = 'transform 0.5s ease';
+    arrow.addEventListener('click', () => {
+        if (rotationState === 0) {
+            rotationState = 1;
+            arrow.style.transform = "rotate(90deg)";
+            search.style.transform = "translateY(0px)";
+            // list.style.transform = "translateY(0px)";
+            products.style.top=220+'px';
+            nav.style.boxShadow='rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;'
+        } else{
+            rotationState = 0;
+            arrow.style.transform = "rotate(0deg)";
+            // list.style.transform = 'translateY(-120px)';
+            products.style.top = h - 50+'px';
+            search.style.transform = 'translateY(-90px)';
+        } 
+
+    });
+
+}
